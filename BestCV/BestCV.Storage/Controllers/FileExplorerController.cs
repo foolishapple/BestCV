4382de64c7904bd688cc24983577f09d150a4068
minh <fsapple12347@gmail.com>
2023-12-09 14:57:52 +0700

    Controller for storage

---
+++ BestCV/BestCV.Storage/Controllers/FileExplorerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BestCV.Storage.Controllers
+{
+    [ApiController]
+    [Route("storage/file")]
+    public class FileExplorerController : Controller
+    {
+
+    }
+}

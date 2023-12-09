83625083542ed0e7c1b687cb75e7b0c3c7baf8f1
minh <fsapple12347@gmail.com>
2023-12-09 10:48:14 +0700

    Commit interface of core

---
+++ BestCV/BestCV.Core/Entities/Interfaces/IEntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestCV.Core.Entities.Interfaces
+{
+    public interface IEntityBase<T>
+    {
+        T Id { get; set; }
+        bool Active { get; set; }
+        DateTime CreatedTime { get; set; }
+    }
+}
---
+++ BestCV/BestCV.Core/Entities/Interfaces/IEntityCommon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestCV.Core.Entities.Interfaces
+{
+    public interface IEntityCommon
+    {
+        string Name { get; set; }
+        string? Description { get; set; }
+    }
+}

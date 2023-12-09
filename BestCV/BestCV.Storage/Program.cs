b6a8541b0c51963b8a0445ceb4b8ab569793fbf6
minh <fsapple12347@gmail.com>
2023-12-09 15:38:22 +0700

    Commit ultilities for storage + appsettings

---
+++ BestCV/BestCV.Storage/Program.cs
@@ -0,0 +1,37 @@
+using BestCV.Storage.Utilities;
+using BestCV.Application;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add seri log
+Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                .CreateLogger();
+Log.Information($"Start {builder.Environment.ApplicationName} up.");
+
+try
+{
+    // Add services to the container.
+    builder.Services
+        .AddInfrastructure(builder.Configuration);
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+
+    app.UseInfrastructure();
+}
+catch (Exception ex)
+{
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal)) throw;
+    Log.Fatal(ex, $"Unhanded exception: {ex.Message}");
+}
+finally
+{
+    Log.Information($"Shut down {builder.Environment.ApplicationName} complete.");
+    Log.CloseAndFlush();
+}

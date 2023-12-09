8b29b453e3d9415c858a3fe16b7c1721af56a700
minh <fsapple12347@gmail.com>
2023-12-09 16:56:15 +0700

    Add view of HomeController(Fixed text) and utilities and launchSettings.json

---
+++ BestCV/BestCV.Web/Utilities/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.IdentityModel.Tokens;
+using BestCV.Core.Entities;
+using System.Text;
+
+namespace BestCV.Web.Utilities
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews()
+                .ConfigureApiBehaviorOptions(opt =>
+                {
+                    opt.SuppressModelStateInvalidFilter = true;
+                })
+                .AddSessionStateTempDataProvider();
+            return services;
+        }
+    }
+}

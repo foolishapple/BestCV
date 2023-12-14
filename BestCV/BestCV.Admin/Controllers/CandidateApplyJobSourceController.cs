817b1658a350b80c8d1001de1a69d550aedbb6e2
minh <fsapple12347@gmail.com>
2023-12-15 00:58:04 +0700

    Commit admin candidate controller

---
+++ BestCV/BestCV.Admin/Controllers/CandidateApplyJobSourceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestCV.Admin.Controllers
+{
+    [Route("candidate-apply-job-source")]
+    [Authorize]
+    public class CandidateApplyJobSourceController : BaseController
+    {
+        private readonly ILogger<CandidateApplyJobSourceController> _logger;
+        public CandidateApplyJobSourceController(ILogger<CandidateApplyJobSourceController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("list")]
+        public IActionResult AdminList()
+        {
+            return View();
+        }
+    }
+}
---
+++ BestCV/BestCV.Admin/Controllers/CandidateApplyJobStatusController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestCV.Admin.Controllers
+{
+    [Route("candidate-apply-job-status")]
+    [Authorize]
+    public class CandidateApplyJobStatusController : BaseController
+    {
+        private readonly ILogger<CandidateApplyJobStatusController> _logger;
+
+        public CandidateApplyJobStatusController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CandidateApplyJobStatusController>();
+        }
+        [Route("list")]
+        public IActionResult AdminList()
+        {
+            return View();
+        }
+    }
+}
